1af083aebd59414612ac9b8926bed4604a72c926
{NAME} <{EMAIL}>
2012-12-19 20:27:20 +0000

    Método de Encoding de vídeo
    Criado o método da classe de view 'encode' que comunica através da
    biblioteca zencode com o serviço de encoding de vídeo autenticado
    do zencoder
    
    Ao final o vídeo convertido é gravado no amazon s3. O vídeo é
    alterado para público ao final da operação.
    
    Caso tudo ocorra com sucesso, o usuário é redirecionado para a
    view de videos que exibe o vídeo encodado.

--- src/dvcoder/dvcoder/urls.py
+++ src/dvcoder/dvcoder/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from encoder.views import EncoderView
-
+from video.views import VideoView
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', EncoderView.as_view(), name='encoder'),
+
+    url(r'^video/(?P<video>.*)$', VideoView.as_view(), name='video'),
     # url(r'^dvcoder/', include('dvcoder.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- src/dvcoder/encoder/views.py
+++ src/dvcoder/encoder/views.py
@@ -11,13 +11,22 @@ from boto import connect_s3
 from boto.s3.key import Key
 from os import remove
 from os.path import join
+from zencoder import Zencoder
+import time
 
 
 class EncoderView(View):
 
+    # nome arquivo de entrada 
+    filename = None
+    # atributo da classe que guarda o nome do vídeo encodado
+    video_name = None
+    # bucket connection
+    bucket = None
 
     def get(self, request):
-        return render_to_response('encoder.html',
+
+	return render_to_response('encoder.html',
                context_instance=RequestContext(request))
 
     def post(self, request):
@@ -28,18 +37,64 @@ class EncoderView(View):
 
         try:
             self.push_to_s3(filepath)
+
+	    if not self.filename or not self.bucket:
+	        raise Exception('Falha ao fazer upload de arquivo')
+
+	    self.encode()
+
         except Exception as s3Exception:
             raise s3Exception
 
-        return redirect('/')
+	return redirect('/video/%s/' % self.video_name)
+
 
     def push_to_s3(self, filepath):
 
-        filename = filepath.split("/")[-1]
+        self.filename = filepath.split("/")[-1]
         boto = connect_s3()
-        bucket = boto.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
-        mykey = Key(bucket)
-        mykey.key = "videos/%s" % filename
+        self.bucket = boto.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
+        mykey = Key(self.bucket)
+        mykey.key = "videos/%s" % self.filename
         mykey.set_contents_from_filename(filepath)
         mykey.make_public()
         remove(filepath)
+
+
+    def encode(self):
+
+	zen = Zencoder('75ffab0006cbaac5e293fa40e80647d0')
+	input_url = 's3://dvcoder/videos/%s' % self.filename
+
+	try:
+	    cut_point = self.filename.rindex('.')
+	    output_filename = self.filename[0:cut_point]
+	except ValueError:
+	    output_filename = self.filename
+
+	video = {'label': 'sambatech',
+  	         'url': 's3://dvcoder/encoded/%s.mp4' % output_filename,
+	         'width': 480,
+	         'height': 320}
+	outputs = (video)
+	self.video_name = "%s.mp4" % output_filename
+
+	job = zen.job.create(input_url, outputs=outputs)
+	job_id = job.body['id']
+
+	progress = zen.job.details(job_id)
+	state = progress.body['job']['state']
+	while state in ('pending', 'waiting', 'processing'):
+	    time.sleep(5)
+	    progress = zen.job.details(job_id)
+	    state = progress.body['job']['state']
+	    print state
+	
+	if state != 'finished':
+	    print "Nurse, we have a problem: ", progress
+	    self.video_name = None
+
+	else:
+            newkey = Key(self.bucket)
+	    newkey.key = "encoded/%s" % self.video_name
+	    newkey.make_public()
